99754a5059f8567c8afb482ecf2d689117e16b45
RudenokArt <rudenokart@yandex.ru>
2022-08-26 08:35:20 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="container pt-5">
+    <div class="row">
+      <div class="col-2 left-nav-bar">
+        <ul class="list-group">
+          <li class="list-group-item p-0" aria-current="true" to="/">
+            <router-link to="/">Задачи</router-link>
+          </li>
+          <li class="list-group-item p-0">
+            <router-link to="/crm">CRM</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  .left-nav-bar a {
+    color: grey;
+    text-decoration: none;
+    display: block;
+    height: 100%;
+    font-weight: 100%;
+    outline: none;
+    padding: 5px;
+  }
 
-nav {
-  padding: 30px;
-}
+  li a.router-link-exact-active {
+    color: red;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
